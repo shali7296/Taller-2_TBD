62efdd160392d6f32931366b7c5599879069bdd1
shali7296 <shalini.ramchandani@usach.cl>
2017-09-08 16:50:15 -0300

    segundo commit

---
+++ plantilla-vue/src/PostActors.vue
@@ -0,0 +1,16 @@
+<template>
+<form class = "center">
+    <h2>Agregar Actores</h2>
+    <div class="emmm">
+        Nombre:
+        <input type = "text"><br><br>
+        Apellido:
+        <input type = ""><br>
+        <br>
+        <button class = "button">Registar Actor</button>
+    </div>
+  </form>
+</template>
+<script>  
+    
+</script>
