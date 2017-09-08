35bfa157ad7a364032409289a61b8e09ba5b3592
shali7296 <shalini.ramchandani@usach.cl>
2017-09-08 19:55:25 -0300

    commit final

--- plantilla-vue/src/PostActors.vue
+++ plantilla-vue/src/PostActors.vue
@@ -1,16 +1,42 @@
 <template>
-<form class = "center">
-    <h2>Agregar Actores</h2>
-    <div class="emmm">
-        Nombre:
-        <input type = "text"><br><br>
-        Apellido:
-        <input type = ""><br>
-        <br>
-        <button class = "button">Registar Actor</button>
-    </div>
-  </form>
+<div class= "center">
+<h2>Agregar Actores</h2>
+  <div class="emmm">
+      Nombre:
+      <input type = "text" v-model="Actor.firstName"><br><br>
+      Apellido:
+      <input type = "text" v-model ="Actor.lastName"><br>
+      <br>
+      <button v-on:click="holaa">Registrar</button>
+  </div>
+</div>
+
 </template>
-<script>  
-    
+
+<script>
+export default {
+  data(){
+    return{
+      Actor: {
+        firstName:'',
+        lastName: ''
+      },
+      alert: 'Holaa'
+    }
+  },
+  methods: {
+    holaa:function(){
+      if(this.Actor.firstName == ''){
+      }
+      else{
+        if(this.Actor.lastName == ''){
+
+        }
+        else{
+        this.$http.post('http://localhost:8081/sakila-spring-backend/actors/', this.Actor)
+        }
+      }
+    }
+  }
+}
 </script>
